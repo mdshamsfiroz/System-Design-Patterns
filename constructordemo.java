d407c7368ac6b2320fd3bf6f12a4bfe18cacd1c3
Md Shams Firoz <firozsaif01@gmail.com>
2023-04-12 22:05:14 +0530

    first commit

---
+++ constructordemo.java
@@ -0,0 +1,20 @@
+import.java.util.*;
+
+public class constructor{
+public static void main(String [] args){
+}
+}[A[D
+[A
+
+[A[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
+
+
+[[A[
+[A[3~[[A[A[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
+private String type;
+
+public constructor(String type){{
+[A[A[B[[B[[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[
+     this.type =type;
+}
+}
