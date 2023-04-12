d510d3d82ff5f4c596bccb19d41b4d9f04c85c98
Md Shams Firoz <76830065+mdshamsfiroz@users.noreply.github.com>
2023-04-13 01:12:08 +0530

    day 1 of learning sd :fire

---
+++ AirCraft.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+   private String type;
+   private String color;
+
+   public Aircraft(String type){
+   this.type=type;
+   }
+
+
+   public Aircraft(String type, String color){
+     this.type = type;
+     this.color = color;
+   }
+}
